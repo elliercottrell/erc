b9418f2ebdf13947e289066cb822846385f568ea
elliercottrell <85800129+elliercottrell@users.noreply.github.com>
2024-02-18 18:21:45 +0000

    Fixing form alignment

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -19,9 +19,9 @@ function Header() {
           <h2>Hiya, welcome to my playground</h2>
           <p>
             This is where I put all of my exploring, so play around. Just hit an
-            image to see its collection,or to make it bigger or smaller, and hit
-            the ERC to come back from anywhere. Drop me a message if you like
-            anything..
+            image to see its collection, or to make it bigger or smaller, and
+            hit the ERC to come back from anywhere. Drop me a message if you
+            like anything..
           </p>
         </Popup>
       </div>
