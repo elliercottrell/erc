0f1178fba8b83d3e284de8a4ac0a938bfdf8d8a7
elliercottrell <85800129+elliercottrell@users.noreply.github.com>
2024-02-18 17:50:54 +0000

    Updated layout

--- src/App.js
+++ src/App.js
@@ -4,32 +4,12 @@ import HomeGallery from "./Components/HomeGallery";
 import Footer from "./Components/Footer";
 import Contact from "./Components/Contact";
 import Form from "./Components/Form";
-import Popup from "./Components/Popup";
-import { useState } from "react";
 
 function App() {
-  const [buttonPopup, setButtonPopup] = useState(false);
-
   return (
     <div className="App">
       <Header />
       <div className="galleryWrap">
-        <div className="myPopup">
-          <button className="open-btn" onClick={() => setButtonPopup(true)}>
-            Hit me
-          </button>
-
-          <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
-            <h2>Hiya, welcome to my playground</h2>
-            <p>
-              This is where I put all of my experimenting, so play around. Just
-              hit an image to see its collection,or to make it bigger or
-              smaller, and hit the ERC to come back from anywhere. Drop me a
-              message if you like anything..
-            </p>
-          </Popup>
-        </div>
-
         <HomeGallery />
       </div>
       <Contact />
