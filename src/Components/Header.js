ecf111c1612b4758e1817fb43f46083cd0251b70
elliercottrell <85800129+elliercottrell@users.noreply.github.com>
2024-02-24 15:48:03 +0000

    moving popup button to it's own component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,30 +1,14 @@
 import { Link } from "react-router-dom";
 import "./Header.css";
-import Popup from "./Popup";
-import { useState } from "react";
+import PopupButton from "./PopupButton";
 
 function Header() {
-  const [buttonPopup, setButtonPopup] = useState(false);
   return (
     <div className="headerWrap">
       <Link to="/" className="ercTitleLink">
         <h1 className="ercTitle">ERC</h1>
       </Link>
-      <div className="myPopup">
-        <button className="open-btn" onClick={() => setButtonPopup(true)}>
-          Help
-        </button>
-
-        <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
-          <h2>Hiya, welcome to my playground</h2>
-          <p>
-            This is where I put all of my exploring, so play around. Just hit an
-            image to see its collection, or to make it bigger or smaller, and
-            hit the ERC to come back from anywhere. Drop me a message if you
-            like anything..
-          </p>
-        </Popup>
-      </div>
+      <PopupButton></PopupButton>
     </div>
   );
 }
---
+++ src/Components/PopupButton.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Popup.css";
+import Popup from "./Popup";
+import { useState } from "react";
+
+function PopupButton() {
+  const [buttonPopup, setButtonPopup] = useState(false);
+  return (
+    <div className="myPopup">
+      <button className="open-btn" onClick={() => setButtonPopup(true)}>
+        Help
+      </button>
+
+      <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+        <h2>Hiya, welcome to my playground</h2>
+        <p>
+          This is where I put all of my exploring, so play around. Just hit an
+          image to see its collection, or to make it bigger or smaller, and hit
+          the ERC to come back from anywhere. Drop me a message if you like
+          anything..
+        </p>
+      </Popup>
+    </div>
+  );
+}
+
+export default PopupButton;
