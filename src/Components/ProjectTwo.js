a08cde72cc934946b3a768396480bb7edbb63464
elliercottrell <85800129+elliercottrell@users.noreply.github.com>
2024-07-06 15:50:59 +0100

    Update ProjectTwo.js

--- src/Components/ProjectTwo.js
+++ src/Components/ProjectTwo.js
@@ -5,15 +5,19 @@ function ProjectTwo() {
   const galleryImagesTwo = [
     {
       img: "Images/model/atOnceTwoThings.jpeg",
+      title: "At Once, Two Things",
     },
     {
       img: "Images/model/inPower.jpg",
+      title: "Wilding",
     },
     {
       img: "Images/model/miumiutwo.jpeg",
+      title: "Untitled",
     },
     {
       img: "Images/model/TimMauricio.jpg",
+      title: "Tim/Mauricio",
     },
     {
       img: "Images/model/timsofa.jpeg",
@@ -21,36 +25,47 @@ function ProjectTwo() {
     },
     {
       img: "Images/model/Self portrait of someone else.jpeg",
+      title: "Self Portrait of Someone Else",
     },
     {
       img: "Images/model/RUD.jpeg",
+      title: "Rapid, Unscheduled, Disassembly",
     },
     {
       img: "Images/model/how to eat the sun.jpeg",
+      title: "How To Eat The Sun",
     },
     {
       img: "Images/model/intothesun.jpeg",
+      title: "Into The Sun I",
     },
     {
       img: "Images/model/untitled.png",
+      title: "Untitled",
     },
     {
       img: "Images/model/intothesuntwo.jpg",
+      title: "Into The Sun II",
     },
     {
       img: "Images/model/Marble.jpg",
+      title: "Marble",
     },
     {
       img: "Images/model/timpink.jpeg",
+      title: "Pink, Tim",
     },
     {
       img: "Images/model/onthegrass.jpeg",
+      title: "On The Grass",
     },
     {
       img: "Images/model/miumiuone.jpeg",
+      title: "Untitled",
     },
     {
       img: "Images/model/onfriday.jpeg",
+      title: "On Friday",
     },
   ];
 
