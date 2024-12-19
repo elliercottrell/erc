5eea1ab365fb46a944e4e8d61fae569b2415aeba
elliercottrell <85800129+elliercottrell@users.noreply.github.com>
2024-12-19 09:38:47 +0000

    updated images

--- src/Components/Drawing.js
+++ src/Components/Drawing.js
@@ -8,48 +8,34 @@ function Drawing() {
       title: "At Once, Two Things",
     },
     {
-      img: "Images/model/inPower.jpg",
-      title: "Wilding",
-    },
-    {
-      img: "Images/model/untitled.jpg",
-      title: "Untitled 🔴",
-    },
-    {
-      img: "Images/model/intothesun.jpeg",
-      title: "Into The Sun I",
+      img: "Images/model/Demaphon falls, in the grass.jpg",
+      title: "Apollo falls, in the grass",
     },
     {
       img: "Images/model/wildMultiplicity.jpeg",
       title: "Wild Multiplicity",
     },
     {
-      img: "Images/model/how to eat the sun.jpeg",
-      title: "How To Eat The Sun",
+      img: "Images/model/whereUndineLastWasSeen.jpg",
+      title: "Where Undine Last Was Seen",
     },
-    {
-      img: "Images/model/Demaphon falls, in the grass.jpg",
-      title: "Demaphon falls, in the grass",
-    },
-    {
-      img: "Images/model/livingInsideOutWeb.jpeg",
-      title: "Living Inside Out",
-    },
+
     {
       img: "Images/model/timsofa.jpeg",
       title: "A portrait of Tim, surrounded by the sofa",
     },
     {
-      img: "Images/model/whereUndineLastWasSeen.jpg",
-      title: "Where Undine Last Was Seen",
+      img: "Images/model/how to eat the sun.jpeg",
+      title: "How To Eat The Sun",
     },
     {
-      img: "Images/model/tuckedBehindYourLiver.jpeg",
-      title: "Tucked Behind Your Liver",
+      img: "Images/model/livingInsideOutWeb.jpeg",
+      title: "Living Inside Out",
     },
+
     {
-      img: "Images/model/Self portrait of someone else.jpeg",
-      title: "Self Portrait of Someone Else 🔴",
+      img: "Images/model/intothesun.jpeg",
+      title: "Into The Sun I",
     },
   ];
 
