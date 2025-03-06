e31c4c748b19f148b8d4c900d45abf2cfca5f829
elliercottrell <85800129+elliercottrell@users.noreply.github.com>
2025-03-06 17:18:01 +0000

    new images

--- src/Components/Drawing.js
+++ src/Components/Drawing.js
@@ -4,38 +4,36 @@ import "./Drawing.css";
 function Drawing() {
   const drawingGalleryImages = [
     {
-      img: "Images/model/atOnceTwoThings.jpeg",
-      title: "At Once, Two Things",
+      img: "Images/model/otherwiseWeAreLost.jpg",
+      title: "The Rite of Spring: Otherwise We Are Lost",
     },
     {
-      img: "Images/model/Apollo falls, in the grass.jpg",
-      title: "Apollo Falls, In The Grass",
+      img: "Images/model/inWhatMovesThem.jpg",
+      title: "The Rite of Spring: In What Moves Them",
+    },
+    {
+      img: "Images/model/pulledBackToTheEarth.jpg",
+      title: "The Rite of Spring: Pulled Back To The Earth",
+    },
+    {
+      img: "Images/model/livingInsideOutWeb.jpeg",
+      title: "Living Inside Out",
     },
     {
       img: "Images/model/wildMultiplicity.jpeg",
       title: "Wild Multiplicity",
     },
     {
-      img: "Images/model/whereUndineLastWasSeen.jpg",
-      title: "Where Undine Last Was Seen",
-    },
-
-    {
-      img: "Images/model/timsofa.jpeg",
-      title: "A portrait of Tim, surrounded by the sofa",
-    },
-    {
       img: "Images/model/how to eat the sun.jpeg",
       title: "How To Eat The Sun",
     },
     {
-      img: "Images/model/livingInsideOutWeb.jpeg",
-      title: "Living Inside Out",
+      img: "Images/model/Apollo falls, in the grass.jpg",
+      title: "Apollo Falls, In The Grass",
     },
-
     {
-      img: "Images/model/intothesun.jpeg",
-      title: "Into The Sun I",
+      img: "Images/model/whereUndineLastWasSeen.jpg",
+      title: "Where Undine Last Was Seen",
     },
   ];
 
